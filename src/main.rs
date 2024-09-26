098f03a4ce289a0f72ad29fb0bbdfedf8da9793c
txaty <txaty@proton.me>
2024-09-26 14:51:20 +0800

    Update benchmark

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use halo2_proofs::circuit::{Layouter, SimpleFloorPlanner};
-use halo2_proofs::dev::MockProver;
 use halo2_proofs::plonk::{create_proof, keygen_pk, keygen_vk, verify_proof, Circuit, ConstraintSystem, Error};
 use halo2_proofs::poly::commitment::ParamsProver;
 use halo2_proofs::poly::kzg::commitment::{
@@ -9,7 +8,6 @@ use halo2_proofs::poly::kzg::multiopen::{ProverSHPLONK, VerifierSHPLONK};
 use halo2_proofs::poly::kzg::strategy::SingleStrategy;
 use halo2_proofs::transcript::{Blake2bRead, Blake2bWrite, Challenge255, TranscriptReadBuffer, TranscriptWriterBuffer};
 use halo2curves::bn256::{Bn256, Fr as Fp, G1Affine};
-use halo2curves::ff::PrimeField;
 use poseidon_circuit::hash::{PoseidonHashTable, SpongeChip, SpongeConfig};
 use poseidon_circuit::poseidon::Pow5Chip;
 use poseidon_circuit::DEFAULT_STEP;
@@ -46,37 +44,42 @@ impl Circuit<Fp> for TestCircuit {
 }
 
 fn main() {
-    let k = 8;
+    let k = 9;
+    let calcs = 140*1024+1;
 
     let params = Params::<Bn256>::unsafe_setup(k);
     let os_rng = ChaCha8Rng::from_seed([101u8; 32]);
     let mut transcript = Blake2bWrite::<_, G1Affine, Challenge255<_>>::init(vec![]);
 
+    let inputs = (0..calcs - 1)
+        .into_iter()
+        .map(|i| {
+            [
+                Fp::from(i as u64),
+                Fp::from((i + 1) as u64),
+            ]
+        })
+        .collect::<Vec<_>>();
+
+    let controls = vec![13; calcs - 1];
+
     let circuit = TestCircuit(
         PoseidonHashTable {
-            inputs: vec![
-                [
-                    Fp::from_str_vartime("1").unwrap(),
-                    Fp::from_str_vartime("2").unwrap(),
-                ],
-                [
-                    Fp::from_str_vartime("30").unwrap(),
-                    Fp::from_str_vartime("1").unwrap(),
-                ],
-                [Fp::from_str_vartime("65536").unwrap(), Fp::zero()],
-            ],
-            controls: vec![0, 46, 14],
+            inputs,
+            controls,
             ..Default::default()
         },
-        4,
+        calcs,
     );
 
-    let prover = MockProver::run(k, &circuit, vec![]).unwrap();
-    assert_eq!(prover.verify(), Ok(()));
 
+    let curr_time = std::time::Instant::now();
     let vk = keygen_vk(&params, &circuit).unwrap();
     let pk = keygen_pk(&params, vk, &circuit).unwrap();
+    println!("keygen time: {:?}", curr_time.elapsed());
 
+
+    let curr_time = std::time::Instant::now();
     create_proof::<KZGCommitmentScheme<Bn256>, ProverSHPLONK<'_, Bn256>, _, _, _, _>(
         &params,
         &pk,
@@ -86,14 +89,13 @@ fn main() {
         &mut transcript,
     )
         .unwrap();
-
     let proof_script = transcript.finalize();
+    println!("Proof generation time: {:?}", curr_time.elapsed());
+
+    let curr_time = std::time::Instant::now();
     let mut transcript = Blake2bRead::<_, _, Challenge255<_>>::init(&proof_script[..]);
     let verifier_params: ParamsVerifier<Bn256> = params.verifier_params().clone();
     let strategy = SingleStrategy::new(&params);
-    let circuit = TestCircuit(PoseidonHashTable::default(), 4);
-    let vk = keygen_vk(&params, &circuit).unwrap();
-
     assert!(
         verify_proof::<KZGCommitmentScheme<Bn256>, VerifierSHPLONK<'_, Bn256>, _, _, _>(
             &verifier_params,
@@ -104,4 +106,5 @@ fn main() {
         )
             .is_ok()
     );
+    println!("Proof verification time: {:?}", curr_time.elapsed());
 }
